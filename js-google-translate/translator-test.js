d75f8d77c4628695c194cd75998e3f3432bca3cb
Lionel Nguyen <lionel.nguyen1812@gmail.com>
2014-05-29 11:52:07 +0700

    chức năng ghép url hoạt động với query là chữ cái nhật

--- js-google-translate/translator-test.js
+++ js-google-translate/translator-test.js
@@ -1,9 +1,49 @@
-test( "translator return chao when translate hello", function() {
-	var translator = new Translator("en", "vi");
-	var query = "hello";
+function causeNotEquals(expected, actual){
+	return "expected \"" + expected + "\" but was \"" + actual + "\"";
+}
 
-	var expect = "chao";
-	var actual = translator.translate(query);
+test( "[test Defined]: Translator ", function() {
+	var translator = new Translator();
+	ok(translator != null, "Translator not defined!");
+});	
 
-	equal(actual, expect , "Dich sai");
+test( "[test Defined]: GoogleTranslateUrlBuilder ", function() {
+	var urlBuilder = new GoogleTranslateURLBuilder();
+	ok(urlBuilder != null, "GoogleUrlBuilder defined!");
+});	
+
+test( "[test Output]: GoogleUrlBuilder.buildWithQuery [hello]", function() {
+	var target = new GoogleTranslateURLBuilder();
+	target.srcLang = LANGUAGE.AUTO;
+	target.desLang = LANGUAGE.VIETNAMESE;
+
+
+	var expected = "http://translate.google.com/translate_a/t?client=webapp&sl=auto&tl=vi&hl=auto&sc=1&q=hello";
+	var actual = target.buildWithQuery("hello");
+
+	ok(expected == actual, causeNotEquals(expected, actual));
+});	
+
+test( "[test Output]: GoogleUrlBuilder.buildWithQuery [hello world]", function() {
+	var target = new GoogleTranslateURLBuilder();
+	target.srcLang = LANGUAGE.AUTO;
+	target.desLang = LANGUAGE.VIETNAMESE;
+
+
+	var expected = "http://translate.google.com/translate_a/t?client=webapp&sl=auto&tl=vi&hl=auto&sc=1&q=hello%20world";
+	var actual = target.buildWithQuery("hello world");
+
+	ok(expected == actual, causeNotEquals(expected, actual));
+});	
+
+test( "[test Output]: GoogleUrlBuilder.buildWithQuery [ありがとう]", function() {
+	var target = new GoogleTranslateURLBuilder();
+	target.srcLang = LANGUAGE.AUTO;
+	target.desLang = LANGUAGE.VIETNAMESE;
+
+
+	var expected = "http://translate.google.com/translate_a/t?client=webapp&sl=auto&tl=vi&hl=auto&sc=1&q=%E3%81%82%E3%82%8A%E3%81%8C%E3%81%A8%E3%81%86";
+	var actual = target.buildWithQuery("ありがとう");
+
+	ok(expected == actual, causeNotEquals(expected, actual));
 });	
--- js-google-translate/translator.js
+++ js-google-translate/translator.js
@@ -8,14 +8,92 @@ var AGENT = {
 	IPHONE3: "Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_0 like Mac OS X; en-us) AppleWebKit/528.18(KHTML, like Gecko) Version/4.0 Mobile/7A341 Safari/528.16"
 }
 
+function GoogleTranslateURLBuilder() {
+	this.mainURL = "http://translate.google.com/translate_a/t?";
+	this.srcLang = "auto";
+	this.desLang = "vi";
 
-function Translator(srcLang, tarLang){
-	this.googleTranslatorURL = "https://translate.google.com.vn/#?/?/?";
-	this.srcLang = srcLang;
-	this.tarLang = tarLang;
+	this.buildWithQuery = function(query) {
+		var url = [];
+		var queryEncoded = encodeURIComponent(query);
+		url.push(
+			"http://translate.google.com/translate_a/t?",
+			"client=webapp",
+			"&sl=auto",
+			"&tl=", this.desLang,
+			"&hl=", this.srcLang,
+			"&sc=1",
+			"&q=", queryEncoded
+			);
+		
+		return url.join("");
+	}
+}
+
+function Translator(){
+	this.srcLang = LANGUAGE.AUTO;
+	this.tarLang = LANGUAGE.VIETNAMESE;
+	this.translateURL = "http://translate.google.com/translate_a/t?client=t&sl=en&tl=es&hl=en&sc=2&ie=UTF-8&oe=UTF-8&oc=1&otf=1&ssel=0&tsel=0";
+
+
 
 	this.translate = function (query){
-		if (src == LANGUAGE.ENGLISH)
-		return "chao";
+		var url = this.translateURL + "&q=" + query;
+
+
 	}
+}
+
+
+
+function httpGet(url)
+{
+    if (window.XMLHttpRequest)
+    {// code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp=new XMLHttpRequest();
+    }
+    else
+    {// code for IE6, IE5
+        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange=function()
+    {
+        if (xmlhttp.readyState==4 && xmlhttp.status==200)
+        {
+            return xmlhttp.responseText;
+        }
+    }
+    xmlhttp.open("GET", theUrl, false );
+    xmlhttp.send();    
+}
+
+function getPage() {
+  // the file to be read
+  pageURL = new java.net.URL ("http://www.yourserver.com/yoyrpage.html");
+
+  // step 1, open the URL
+  var openConnection = pageURL.openConnection;
+  theConnection = openConnection()
+
+  // step 2, connect to server
+  var t=theConnection.connect
+  t()
+
+  // step 3, read the file using HTTP protocol
+  var getContent = theConnection.getContent
+  var theURLStream = getContent()
+
+  // step 4, get an handle and fetch the content length
+  var readStream = theURLStream.read
+  var gcl = theConnection.getContentLength
+  gcLen = gcl()
+
+  // and finally, read into a variable
+  theText =""
+  for (i = 1; i <gcLen; i++) {
+   theText += new java.lang.Character(readStream())
+   }
+
+  // for demonstration
+  alert(theText)
 }

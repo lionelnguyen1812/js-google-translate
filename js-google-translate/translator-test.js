af9bf235bb2e05a5b093a9c8ad13c7b9ec172b26
Lionel Nguyen <lionel.nguyen1812@gmail.com>
2014-05-29 12:01:11 +0700

    bỏ class GoogleURLBUild, đưa luôn chức năng buildURL vào class Translator

--- js-google-translate/translator-test.js
+++ js-google-translate/translator-test.js
@@ -7,43 +7,39 @@ test( "[test Defined]: Translator ", function() {
 	ok(translator != null, "Translator not defined!");
 });	
 
-test( "[test Defined]: GoogleTranslateUrlBuilder ", function() {
-	var urlBuilder = new GoogleTranslateURLBuilder();
-	ok(urlBuilder != null, "GoogleUrlBuilder defined!");
-});	
 
-test( "[test Output]: GoogleUrlBuilder.buildWithQuery [hello]", function() {
-	var target = new GoogleTranslateURLBuilder();
+test( "[test Output]: buildURL with query [hello]", function() {
+	var target = new Translator();
 	target.srcLang = LANGUAGE.AUTO;
 	target.desLang = LANGUAGE.VIETNAMESE;
 
 
 	var expected = "http://translate.google.com/translate_a/t?client=webapp&sl=auto&tl=vi&hl=auto&sc=1&q=hello";
-	var actual = target.buildWithQuery("hello");
+	var actual = target.buildURL("hello");
 
 	ok(expected == actual, causeNotEquals(expected, actual));
 });	
 
-test( "[test Output]: GoogleUrlBuilder.buildWithQuery [hello world]", function() {
-	var target = new GoogleTranslateURLBuilder();
+test( "[test Output]: buildURL with query [hello world]", function() {
+	var target = new Translator();
 	target.srcLang = LANGUAGE.AUTO;
 	target.desLang = LANGUAGE.VIETNAMESE;
 
 
 	var expected = "http://translate.google.com/translate_a/t?client=webapp&sl=auto&tl=vi&hl=auto&sc=1&q=hello%20world";
-	var actual = target.buildWithQuery("hello world");
+	var actual = target.buildURL("hello world");
 
 	ok(expected == actual, causeNotEquals(expected, actual));
 });	
 
-test( "[test Output]: GoogleUrlBuilder.buildWithQuery [ありがとう]", function() {
-	var target = new GoogleTranslateURLBuilder();
+test( "[test Output]: buildURL with query [ありがとう]", function() {
+	var target = new Translator();
 	target.srcLang = LANGUAGE.AUTO;
 	target.desLang = LANGUAGE.VIETNAMESE;
 
 
 	var expected = "http://translate.google.com/translate_a/t?client=webapp&sl=auto&tl=vi&hl=auto&sc=1&q=%E3%81%82%E3%82%8A%E3%81%8C%E3%81%A8%E3%81%86";
-	var actual = target.buildWithQuery("ありがとう");
+	var actual = target.buildURL("ありがとう");
 
 	ok(expected == actual, causeNotEquals(expected, actual));
 });	
--- js-google-translate/translator.js
+++ js-google-translate/translator.js
@@ -8,37 +8,28 @@ var AGENT = {
 	IPHONE3: "Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_0 like Mac OS X; en-us) AppleWebKit/528.18(KHTML, like Gecko) Version/4.0 Mobile/7A341 Safari/528.16"
 }
 
-function GoogleTranslateURLBuilder() {
-	this.mainURL = "http://translate.google.com/translate_a/t?";
-	this.srcLang = "auto";
-	this.desLang = "vi";
-
-	this.buildWithQuery = function(query) {
-		var url = [];
-		var queryEncoded = encodeURIComponent(query);
-		url.push(
-			"http://translate.google.com/translate_a/t?",
-			"client=webapp",
-			"&sl=auto",
-			"&tl=", this.desLang,
-			"&hl=", this.srcLang,
-			"&sc=1",
-			"&q=", queryEncoded
-			);
-		
-		return url.join("");
-	}
-}
-
 function Translator(){
 	this.srcLang = LANGUAGE.AUTO;
 	this.tarLang = LANGUAGE.VIETNAMESE;
-	this.translateURL = "http://translate.google.com/translate_a/t?client=t&sl=en&tl=es&hl=en&sc=2&ie=UTF-8&oe=UTF-8&oc=1&otf=1&ssel=0&tsel=0";
 
-
+	this.buildURL = function(query) {
+			var url = [];
+			var queryEncoded = encodeURIComponent(query);
+			url.push(
+				"http://translate.google.com/translate_a/t?",
+				"client=webapp",
+				"&sl=auto",
+				"&tl=", this.desLang,
+				"&hl=", this.srcLang,
+				"&sc=1",
+				"&q=", queryEncoded
+				);
+			
+			return url.join("");
+		}
 
 	this.translate = function (query){
-		var url = this.translateURL + "&q=" + query;
+		var url = new GoogleTranslateURLBuilder().buildWithQuery(query);
 
 
 	}
